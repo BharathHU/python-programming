9b35c0d1b6f2a584b2b4997e84ee25a708590218
BharathHU <bharathhubharath@gmail.com>
2026-04-08 09:51:35 +0530

    updated

--- distionary.py
+++ distionary.py
@@ -1,8 +1,23 @@
-# WAp to segrigate between duplicate and non duplicate and unique present in an array
-arr=[1,2,2,1,3,1,4]
-for i in range(len(arr)):
-    if arr[i] in arr[:i]+arr[i+1:]:
-        print(f"{arr[i]} is duplicate")
-    else:
-        print(f"{arr[i]} is unique")
-    
+def seggreagation(arr):
+    dup,nondup,unique=[],[],[]
+    dict={}
+    for i in range(0,len(arr)):
+        if arr[i] in dict:
+            dict[arr[i]]+=1
+        else:
+            dict[arr[i]]=1
+    for key ,val in dict.items():
+        if val>1:
+            dup.append(key)
+        if val>=1:
+            nondup.append(key)
+        if val==1:
+            unique.append(key)
+    return dup,nondup,unique
+
+arr=list(map(int,input("Enter a array:").split()))
+print("Orginal array:",arr)
+dup,nondup,unique=seggreagation(arr)
+print("Duplicates:",dup)
+print("Non-Duplicates:",nondup)
+print("Unique:",unique)
